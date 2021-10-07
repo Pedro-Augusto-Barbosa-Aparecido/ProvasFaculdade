1a05af71e2e8c1027487f99128be76c7c9cf7681
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2021-10-06 22:25:33 -0300

    Aritimetica de ponteiros

---
+++ files/aritimetica_de_ponteiro.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int vet[3];
+
+    int *pont = NULL;
+
+    vet[0] = 2;
+    vet[1] = 1;
+    vet[2] = 0;
+
+    cout << vet << endl;
+    cout << *vet << endl;
+
+    pont = vet;
+    cout << *pont << endl;
+    pont++;
+    cout << *pont << endl;
+    pont++;
+    cout << *pont << endl;
+
+    return 8;
+
+}
