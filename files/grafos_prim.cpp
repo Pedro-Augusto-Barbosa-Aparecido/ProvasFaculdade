9c7ff33f183bcd93684b405d9f9eec93a3e66609
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2021-11-10 22:46:19 -0300

    Aula de grafos 'arvore geradora minima'

---
+++ files/grafos_prim.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+struct no {
+    int v; // vertice adjacente
+    int peso; // peso da aresta
+
+};
+
+void cria_aresta(list<no>adj[], int u, int v, int p, int orientado) {
+    no aux;
+
+    aux.peso = p;
+    aux.v = v;
+
+    if (orientado == 1) {
+        adj[u].push_back(aux);
+
+    } else {
+        int o = aux.v;
+        
+        adj[u].push_back(aux);
+
+        aux.v = u;
+
+        adj[o].push_back(aux);
+
+    }
+
+}
+
+void bfs(list<no>adj[], int nVertices, int s) {
+    int state[10], p[10];
+    int u = 0, v = 0;
+
+    list<no>::iterator it;
+    list<int> Q;
+
+    for (int i = 0; i < 10; i++) {
+        state[i] = -1;
+        p[i] = -1;
+
+    }
+
+    state[s] = 0;
+
+    Q.push_back(s);
+
+    while (!Q.empty()) {
+        u = Q.front();
+        Q.pop_front();
+
+        cout << u << endl;
+
+        for (it = adj[u].begin(); it != adj[u].end(); it++) {
+            v = it->v;
+
+            cout << u << " " << v << endl;
+            if (state[v] == -1) {
+                state[v] = 0;
+                p[v] = u;
+
+                Q.push_back(v);
+
+            }
+
+        }
+
+        state[v] = 1;
+
+    }
+
+}
+
+void prim(list<no>adj[], int nVertices, int start) {
+    list<no>::iterator it;
+
+    int parent[100], distance[100];
+    int v = 0;
+
+    int destino = 0, weight = 0; 
+    int dist = INT_MAX;
+
+    int custo = 0;
+
+    bool inTree[100];
+
+    for (int u = 0; u < nVertices; u++) {
+        parent[u] = -1;
+        distance[u] = INT_MAX;
+        inTree[u] = false;
+
+    }
+
+    distance[start] = 0;
+    v = start;
+
+    while (inTree[v] == false) {
+        inTree[v] = true;
+
+        for (it = adj[v].begin(); it != adj[v].end(); it++) {
+            destino = it->v;
+            weight = it->peso;
+
+            if ((distance[destino] > weight) && (inTree[destino] == false)) {
+                distance[destino] = weight;
+                parent[destino] = v;
+
+            }
+
+        }
+
+        v = 0;
+        dist = INT_MAX;
+
+        for (int u = 0; u < nVertices; u++) {
+            if ((inTree[u] == false) && (dist > distance[u])) {
+                dist = distance[u];
+                v = u;
+
+            }
+
+        }
+
+    }
+
+    for (int i = 0; i < nVertices; i++) {
+        custo += distance[i];
+
+        if (parent[i] != -1) 
+            cout << parent[i] << " " << i << endl;
+
+    }
+
+    cout << "Custo: " << custo << endl;
+
+}
+
+int main () {
+
+    list<no> grafo[10]; // grafo original
+
+    int tam = 0; // numero de vertices do grafo
+    int vInicial = 0; // vertice inicial
+
+    int ord = 0; // 1 - orientado 0 - nao orientado
+    int o = 0, v = 0, p = 0; // origem, destino e peso da aresta
+
+    // capitutrando a qtd de vertices, ordem, vertice inicial
+    cin >> tam >> ord >> vInicial;
+
+    // capiturando arestas
+    cin >> o >> v >> p;
+    while ((o != -1) && (v != -1) && (p != -1)) {
+        cria_aresta(grafo, o, v, p, ord);
+
+        cin >> o >> v >> p;
+
+    }
+
+    cout << "Arvore Geradora Minima: \n";
+    prim(grafo, tam, vInicial);
+
+    return 0;
+
+}
